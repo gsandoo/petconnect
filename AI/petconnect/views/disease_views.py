6279e3377e0cf12a4c0251efe3ec1390cc492ad8
HyeonJi Na <dev.hyeonji@gmail.com>
2023-09-24 11:09:00 +0900

    Revert "feat: 서버와 통신"
    This reverts commit 5ad84ecef6e32d878b53f580260029468ba82b52.

--- AI/petconnect/views/disease_views.py
+++ AI/petconnect/views/disease_views.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Blueprint, request, jsonify
 import os
 import cv2
 from skimage.feature import hog
 import pickle
+import pymysql
 
-app = Flask(__name__)
+bp = Blueprint('disease', __name__, url_prefix='/disease')
 
 # 현재 파일의 디렉토리 경로
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -16,36 +17,90 @@ disease_model_path = os.path.join(current_dir, '..', '..', 'disease', 'SVM-Class
 with open(disease_model_path, 'rb') as model_file:
     svm_model = pickle.load(model_file)
 
-# 예측 가능한 질병 카테고리
 categories = ['atopy', 'hotspots', 'hair_loss', 'normal']
 
-@app.route('/disease', methods=['POST'])
+# db
+def db_connector():
+    db_params = {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'Qwer12345678!',
+        'db': 'pet_connect',
+        'charset': 'utf8',
+        'cursorclass': pymysql.cursors.DictCursor
+    }
+    
+    connector = pymysql.connect(**db_params)
+    return connector
+
+@bp.route('/', methods=['POST'])
 def analyze_disease():
     if 'file' not in request.files:
-        return jsonify({'error': '파일 부분이 없습니다'})
+        return jsonify({'error': 'No file part'})
 
     file = request.files['file']
     if file.filename == '':
-        return jsonify({'error': '선택된 파일이 없습니다'})
+        return jsonify({'error': 'No selected file'})
 
     if file:
-        # 업로드된 이미지를 임시 위치에 저장
+        # Create the 'uploads' directory if it doesn't exist
+        if not os.path.exists('uploads'):
+            os.makedirs('uploads')
+
+        # Save the uploaded image to a temporary location
         image_path = os.path.join('uploads', file.filename)
         file.save(image_path)
 
-        # 이미지 로드 및 전처리
+        # Load and preprocess the image
         image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
         preprocessed_image = cv2.resize(image, (64, 64))
         hog_features = hog(preprocessed_image, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1))
 
-        # SVM 모델을 사용하여 질병 카테고리 예측
+        # Predict the disease category using SVM model
         predicted_label = svm_model.predict([hog_features])[0]
         predicted_category = categories[predicted_label]
 
-        # 임시 이미지 파일 삭제
+        # Remove the temporary image file
         os.remove(image_path)
 
-        return jsonify({'result': predicted_category})
+        # Store the result in the database
+        try:
+            db_conn = db_connector()
+            cursor = db_conn.cursor()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+            query = "INSERT INTO disease_results (dogRegistNum, predicted_category) VALUES (%s, %s)"
+            values = (request.form['dogRegistNum'], predicted_category)
+            cursor.execute(query, values)
+            db_conn.commit()
+
+            cursor.close()
+            db_conn.close()
+
+            return jsonify({'result': predicted_category})
+
+        except Exception as e:
+            print("Error storing result in the database:", e)
+            return jsonify({'error': 'Failed to store result in the database'})
+
+@bp.route('/<dogRegistNum>', methods=['GET'])
+def get_disease_result(dogRegistNum):
+    try:
+        db_conn = db_connector()
+        cursor = db_conn.cursor()
+
+        query = "SELECT predicted_category FROM disease_results WHERE dogRegistNum = %s"
+        values = (dogRegistNum,)
+        cursor.execute(query, values)
+        result = cursor.fetchone()
+
+        cursor.close()
+        db_conn.close()
+
+        if result:
+            return jsonify({'result': result['predicted_category']})
+        else:
+            return jsonify({'error': 'No result found'})
+
+    except Exception as e:
+        print("Error retrieving result from database:", e)
+        return jsonify({'error': 'Failed to retrieve result from database'})
--- AI/petconnect/views/eye_views.py
+++ AI/petconnect/views/eye_views.py
@@ -3,6 +3,7 @@ import os
 import cv2
 from skimage.feature import hog
 import pickle
+import pymysql
 
 bp = Blueprint('eye', __name__, url_prefix='/eye')
 
@@ -16,33 +17,90 @@ eye_model_path = os.path.join(current_dir, '..', '..', 'eye', 'SVM-Classifier', 
 with open(eye_model_path, 'rb') as model_file:
     svm_model = pickle.load(model_file)
 
-# 예측 가능한 눈 카테고리
 categories = ['cataracts', 'cherry_eye', 'normal']
+
+# db
+def db_connector():
+    db_params = {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'Qwer12345678!',
+        'db': 'pet_connect',
+        'charset': 'utf8',
+        'cursorclass': pymysql.cursors.DictCursor
+    }
+    
+    connector = pymysql.connect(**db_params)
+    return connector
 
 @bp.route('/', methods=['POST'])
 def analyze_eye():
     if 'file' not in request.files:
-        return jsonify({'error': '파일 부분이 없습니다'})
+        return jsonify({'error': 'No file part'})
 
     file = request.files['file']
     if file.filename == '':
-        return jsonify({'error': '선택된 파일이 없습니다'})
+        return jsonify({'error': 'No selected file'})
 
     if file:
-        # 업로드된 이미지를 임시 위치에 저장
+        # Create the 'uploads' directory if it doesn't exist
+        if not os.path.exists('uploads'):
+            os.makedirs('uploads')
+
+        # Save the uploaded image to a temporary location
         image_path = os.path.join('uploads', file.filename)
         file.save(image_path)
 
-        # 이미지 로드 및 전처리
+        # Load and preprocess the image
         image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
         preprocessed_image = cv2.resize(image, (64, 64))
         hog_features = hog(preprocessed_image, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1))
 
-        # SVM 모델을 사용하여 눈 카테고리 예측
+        # Predict the eye category using SVM model
         predicted_label = svm_model.predict([hog_features])[0]
         predicted_category = categories[predicted_label]
 
-        # 임시 이미지 파일 삭제
+        # Remove the temporary image file
         os.remove(image_path)
 
-        return jsonify({'result': predicted_category})
+        # Store the result in the database
+        try:
+            db_conn = db_connector()
+            cursor = db_conn.cursor()
+
+            query = "INSERT INTO eye_results (dogRegistNum, predicted_category) VALUES (%s, %s)"
+            values = (request.form['dogRegistNum'], predicted_category)
+            cursor.execute(query, values)
+            db_conn.commit()
+
+            cursor.close()
+            db_conn.close()
+
+            return jsonify({'result': predicted_category})
+
+        except Exception as e:
+            print("Error storing result in the database:", e)
+            return jsonify({'error': 'Failed to store result in the database'})
+
+@bp.route('/<dogRegistNum>', methods=['GET'])
+def get_eye_result(dogRegistNum):
+    try:
+        db_conn = db_connector()
+        cursor = db_conn.cursor()
+
+        query = "SELECT predicted_category FROM eye_results WHERE dogRegistNum = %s"
+        values = (dogRegistNum,)
+        cursor.execute(query, values)
+        result = cursor.fetchone()
+
+        cursor.close()
+        db_conn.close()
+
+        if result:
+            return jsonify({'result': result['predicted_category']})
+        else:
+            return jsonify({'error': 'No result found'})
+
+    except Exception as e:
+        print("Error retrieving result from database:", e)
+        return jsonify({'error': 'Failed to retrieve result from database'})
